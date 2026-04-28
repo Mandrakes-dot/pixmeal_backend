e08e34644b0ef68ec4afb1e1965d4f0d745d30a1
tom <tomtom.kazc@hotmail.com>
2026-04-28 15:37:56 +0200

    feature: devops-setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
+  imports: [UsersModule, JwtModule],
   providers: [AuthService],
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersRepository } from './users.repository';
+import { UsersMapper } from './users.mapper';
 
 export const USER_MODEL = 'USER_MODEL';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, UsersRepository, UsersMapper],
+  exports: [UsersService, PrismaService, UsersRepository, UsersMapper],
 })
 export class UsersModule {}
